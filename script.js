5f963397996adaf77686e325076eb821caa500eb
Eric Johnson <johnsoneric586@gmail.com>
2024-06-06 06:43:51 -0400

    Did section 3 exercises - practicing with the spread operator

--- script.js
+++ script.js
@@ -290,3 +290,22 @@ const {
 function printBookInfo({ title, author, year = 'year unknown' }) {
   console.log(`${title} by ${author}, ${year}`);
 }
+
+// ------------------------------------------------
+// The Spread Operator
+
+// 3.1) Each book object has the author property, which stores an array of strings (author names) if there are multiple authors, or a single string (author name) if there is just one author.
+
+// Declare an array called bookAuthors, and fill it with authors of the first two books from the books array. The bookAuthors array should have just one level (no nested arrays).
+
+let bookAuthors = [];
+
+bookAuthors.push(...books[0].author, ...books[1].author);
+
+// 3.2) Write a function called spellWord that accepts a single string as an argument. This function should log to the console each letter of the argument separated by a space.
+
+function spellWord(string) {
+  console.log(...string);
+}
+
+// ------------------------------------------------
