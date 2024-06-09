ee36cb42eb02157376a8940c9a710e947cb5e19c
Eric Johnson <johnsoneric586@gmail.com>
2024-06-09 12:09:44 -0400

    Did exercise 11.2 practicing looping over objects

--- script.js
+++ script.js
@@ -488,3 +488,14 @@ function getFirstKeyword(book) {
 }
 
 // ------------------------------------------------
+// 11.1) Below is the entries variable that stores an empty array. Use the for-of loop together with the Object.keys() method to loop over the thirdParty.goodreads property (array) of the first book object from the books array. For each key, push a new array that contains that key to the entries array.
+
+// In the end, the entries array should be filled with arrays containing keys:
+
+const entries = [];
+
+// So basically we want all the goodreads keys in the entries array..
+
+for (const key of Object.keys(books[0].thirdParty.goodreads)) {
+  entries.push(key);
+}
