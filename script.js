2ebb6cac4964c8d70c43d7ca7cc4e0f3b496c936
Eric Johnson <johnsoneric586@gmail.com>
2024-06-11 07:52:16 -0400

    Did 15.1 to 15.4 exercises - practicing with string methods

--- script.js
+++ script.js
@@ -587,10 +587,39 @@ const firstBookMap = new Map(Object.entries(books[0]));
 
 // 14.2) Use the for-of loop to iterate over firstBookMap, and log to the console keys that have numbers as values.
 
-for (const [key, value] of firstBookMap) {
-  if (typeof value === 'number') {
-    console.log(key);
-  }
-}
+// for (const [key, value] of firstBookMap) {
+//   if (typeof value === 'number') {
+//     console.log(key);
+//   }
+// }
 
 // ------------------------------------------------
+// Working with Strings - Part 1
+
+// 15.1) Take the ISBN property of the first book from the books array, and log to the console characters at index 6, 4, 9 and 8. Use bracket notation to access individual characters.
+
+// console.log(books[0].ISBN[6]);
+// console.log(books[0].ISBN[4]);
+// console.log(books[0].ISBN[9]);
+// console.log(books[0].ISBN[8]);
+
+// 15.2) Below is the quote variable that stores a string. Find the index of the word 'chess', and log it to the console.
+
+const quote =
+  'A computer once beat me at chess, but it was no match for me at kick boxing';
+
+// console.log(quote.indexOf('chess'));
+
+// 15.3) Extract the word "boxing" from the same quote string, and log it to the console.
+
+// console.log(quote.slice(quote.lastIndexOf(' ') + 1));
+
+// 15.4) Some authors are noted as "(Contributor)", for example "Julie Sussman (Contributor)". Create a function called isContributor that takes an author's name as an argument, and returns either true (if he's a contributor) of false (if he's not a contributor). The string "(Contributor)" is always the last part of the author's name string.
+
+// function isContributor(author) {
+//   if (author.slice(author.lastIndexOf(' ') + 1) === '(Contributor)') {
+//     return true;
+//   } else {
+//     return 'Not a contributor.';
+//   }
+// }
